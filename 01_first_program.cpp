d76d36702d2ca262e403d8a0922333d3c2b7e30e
Kelta-King <kushangshah131@gmail.com>
2022-02-08 04:13:42 -0800

    new file

---
+++ 01_first_program.cpp
@@ -0,0 +1,13 @@
+// Pre-processor command
+#include <iostream>
+
+// use of namespace
+using namespace std;
+
+// Main method, entry of code
+int main() {
+    
+    // Printing hello world, here c means console
+    cout << "Hello World!";
+    return 0;
+}
---
+++ 02_comments.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// Main method, entry of code
+int main() {
+    
+    cout << "This is first line" << endl;
+    // cout << "This is second line";
+    cout << "This is third line";
+    return 0;
+}
