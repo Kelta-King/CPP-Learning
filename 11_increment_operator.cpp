13a0a24bfa8924d1b3727b127d0dcea47d74c79c
Kelta-King <kushangshah131@gmail.com>
2022-02-08 21:29:13 -0800

    loops

---
+++ 11_increment_operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i = 0;
+    int j = 0;
+
+    cout << "Incremented i is: " << i++ << endl;
+    cout << "Incremented j is: " << ++j << endl;
+
+}
