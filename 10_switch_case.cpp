4a2d4d3b767ebc7863263763fcfb363884caedd6
Kelta-King <kushangshah131@gmail.com>
2022-02-08 21:18:26 -0800

    switch

--- 10_switch_case.cpp
+++ 10_switch_case.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    
+    int position;
+    cout << "Enter your Position:";
+    cin >> position;
 
+    // Checking the runner's position in race
+    switch (position)
+    {
+    case 1:
+        cout << "You won gold medal";
+        break;
+    case 2:
+        cout << "You won silver medal";
+        break;
+    case 3:
+        cout << "You won bronze medal";
+        break;
+
+    default:
+        cout << "Thanks for participating in race :-)";
+        break;
+    }
 }
